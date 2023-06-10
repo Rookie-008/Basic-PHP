beaef84a49b549a7b7f40cf58844e2cecc933437
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2023-06-10 13:05:08 +0900

    Add files via upload

---
+++ 01Introduction/02BasicCode/hello3.php
@@ -0,0 +1,16 @@
+<?php 
+
+// Print Array
+print_r("Hello Hello");
+print "<br>";
+
+// Check Data Type and Value in Variable
+var_dump("Hello Hello");
+print "<br>";
+var_dump(123);
+print "<br>";
+var_dump(true);
+print "<br>";
+var_dump(false);
+
+?>
