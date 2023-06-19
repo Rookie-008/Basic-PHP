766c2882c7b0f87b4183e36c0bfdfa3e41e66408
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2023-06-20 02:47:32 +0700

    Add files via upload

---
+++ 02Variable&DataType/07 Superglobal/superglobal.php
@@ -0,0 +1,45 @@
+<?php
+// Variabel Superglobals (Variable Special) --> ARRAY ASSOSIATIF
+// 1. $_GET
+// 2. $_POST
+// 3. $_REQUEST
+// 4. $_SESSION
+// 5. $_COOKIE
+// 6. $_SERVER
+// 7. $_ENV  <== Environment
+
+// Variable Scope
+// $x = 10; // <== Local Variable
+// echo $x; // <== Show 10 to Output
+
+// Variable Function
+// function hello() {
+// 	$x = 20;   // <== $x Variable Local
+// 	echo $x;
+// } 
+// hello();
+// echo "<br>";
+// echo $x;
+// ?>
+
+<?php
+// Variabel Scope
+
+//$x = 10;
+
+//function hello() {
+	//global $x;
+	//echo $x;
+//}
+//hello();
+?>
+
+<?php
+// SUPERGLOBALS
+
+// 1. $_GET
+//$_GET["nama"] = "Abc";
+//$_GET["nim"] = "123456";
+var_dump($_GET);
+
+?>
