dc6a20b8029f0c5cdb82dc2cd96399c89ac86035
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2023-06-26 22:24:56 +0700

    Add files via upload

---
+++ 08 Create Database/note.php
@@ -0,0 +1,29 @@
+<?php
+// Database --> Data in DBMS
+
+// DBMS (Database Management System)
+// - Oracle
+// - MySQL
+// - SQL Server 
+// - PostgreSQL
+// - MongoDB
+
+// DBMS Type
+// - Relational DBMS (RDBMS)
+// - Hierarchical DBMS (HDBMS)
+// - Network DBMS (NetDBMS)
+// - NoSQL DBMS --> (MongoDB)
+
+// Primary Key, presentation a data with unique row record.
+
+// Technical term on Database:
+// - Field/Column
+// - Row/Record/Baris
+// - Key
+//    - Primary Key
+//    - Foreign Key
+// - Auto Increment
+// - Relationship
+// - Normalization
+
+?>
