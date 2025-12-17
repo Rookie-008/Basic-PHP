9ab4e0ef3ef4e3840966b3ecf63bb5952a52237a
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-17 18:51:06 +0700

    Add files via upload

--- 03ConditionalStatement/02IfElseIf/ifElseIf.php
+++ 03ConditionalStatement/02IfElseIf/ifElseIf.php
@@ -1,11 +1,13 @@
 <?php  
 
-$a = 1;
+$a = 35;
 
-if($a < 5) {
-	echo "A";
-}else if ($a == 1) {
-	echo "B";
+if($a < 85) {
+	echo "In The End, We Only Regret The Chances We Didnt Take";
+}else if ($a == 15) {
+	echo "You Can Only Protect Your Liberties In This World By Protecting The Other Man Freedom";
 }
 
 ?>
+
+
