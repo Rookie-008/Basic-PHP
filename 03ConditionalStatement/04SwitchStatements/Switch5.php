e82acdad84d87de223417704e38cced4b49d7a82
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-17 18:57:17 +0700

    Add files via upload

---
+++ 03ConditionalStatement/04SwitchStatements/Switch5.php
@@ -0,0 +1,27 @@
+<?php  
+
+$Season = "5";
+
+switch ($Season) {
+	case 'Not Good':
+		echo "In The End, We Only Regret The Chances We Didnt Take";
+		break;
+	case 'Good':
+		echo "Not Everyday Is Good, But There Is Something Good In Everyday";
+		break;
+	case 'Very Good':
+		echo "You Can Only Protect Your Liberties In This World By Protecting The Other Man Freedom";
+		break;
+	case 'Bad':
+		echo "Do Not Believe Everything You Think";
+		break;
+	case 'Very Bad':
+		echo "They Judge Me Before They Even Know Me, Thats Why I am Better Off Alone";
+		break;
+		
+	default:
+		echo "Choose Not Good,Good,Very Good,Bad or Very Bad";
+		break;
+}
+
+?>
