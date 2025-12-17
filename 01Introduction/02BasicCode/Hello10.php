f4628f5d767ffd6b1a98922365331e1f5ceaf7fa
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-17 10:41:58 +0700

    Add files via upload

---
+++ 01Introduction/02BasicCode/Hello10.php
@@ -0,0 +1,25 @@
+<?php
+
+// $1file = "Time Is Precious, Don't Waste It"; // False
+// $file1 = "Time Is Precious, Don't Waste It"; // True
+$file = "Time Is Precious, Don't Waste It";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PHP in HTML</title>
+</head>
+<body>
+
+	<h1>Hello <?php echo "Time Is Precious, Don't Waste It"; ?></h1>
+	<h1>Hello <?php echo $file ?></h1>
+
+	<p><?php echo "If You Love Life, Dont Waste Time"?></p>
+	<p><?php echo "PHP in HTML"?></p>
+
+</body>
+</html>
---
+++ 01Introduction/02BasicCode/Hello8.php
@@ -0,0 +1,16 @@
+<?php 
+
+// Print Array
+print_r("If You Love Life, Dont Waste Time");
+print "<br>";
+
+// Check Data Type and Value in Variable
+var_dump("Time Is Precious, Don't Waste It");
+print "<br>";
+var_dump(2619);
+print "<br>";
+var_dump(true);
+print "<br>";
+var_dump(false);
+
+?>
