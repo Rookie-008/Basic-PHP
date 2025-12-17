bed92c92e671e0f5834fecc32804acb8d55ee2ff
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-17 10:42:54 +0700

    Add files via upload

---
+++ 02Variable&DataType/01DataType/variable2.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PHP</title>
+</head>
+<body>
+
+	<?php  
+	
+		$variable1 = "Don't Waste It";
+		$variable2 = 2619;
+		$variable3 = true;
+
+		echo "Time Is Precious $variable1";
+		echo "<br>";
+		echo $variable2;
+		echo "<br>";
+		echo "Condition $variable3";
+
+	?>
+
+</body>
+</html>
