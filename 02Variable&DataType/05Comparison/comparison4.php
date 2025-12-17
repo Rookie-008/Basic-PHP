b6f8406240b14345e7e629e6676e51baca0658c8
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-17 18:49:44 +0700

    Add files via upload

---
+++ 02Variable&DataType/05Comparison/comparison4.php
@@ -0,0 +1,14 @@
+<?php  
+
+// === (Identical)     --> Returns true if both $a and $b are equal, and have the same data type
+// !== (Not Identical) --> Returns true if both $a and $b not equal, or if they don`t have the same data type
+
+var_dump(20 === "20");
+echo "<br>";
+var_dump(UNKNOWN !== "UNKNOWN");
+echo "<br>";
+var_dump(IMPRESSION !== "IMPRESSION");
+echo "<br>";
+var_dump(1 !== "AWESOME");
+
+?>
