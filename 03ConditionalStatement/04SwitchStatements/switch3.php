efe425cb7d8d0395b657199fa5271eac2054187a
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-18 10:41:03 +0700

    Add files via upload

---
+++ 03ConditionalStatement/04SwitchStatements/switch3.php
@@ -0,0 +1,42 @@
+<?php  
+
+$penyedapRasa = "5";
+
+switch ($Manisan) {
+	case '1':
+		echo "Bombon Keju";
+		break;
+	case '2':
+		echo "Bombon Buah Lunak";
+		break;
+	case '3':
+		echo "Bombon Susu";
+		break;
+	case '4':
+		echo "Bombon Cokelat";
+		break;
+	case '5':
+		echo "Bombon Pedis";
+		break;
+	case '6':
+		echo "Bombon Kacamata";
+		break;
+	case '7':
+		echo "Cokelat Jago";
+		break;
+	case '8':
+		echo "Cokelat Coin";
+		break;
+	case '9':
+		echo "Bombon Jahe";
+		break;
+	case '10':
+		echo "Bombon Kelapa";
+		break;
+
+	default:
+		echo "Choose 1,2,3,4,5,6,7,8,9 or 10";
+		break;
+}
+
+?>
