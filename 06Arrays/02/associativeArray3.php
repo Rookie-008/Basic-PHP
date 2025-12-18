473156ba80a1ee48a57ce58ecfdc22a320b17dfe
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-19 06:17:04 +0700

    Add files via upload

---
+++ 06Arrays/02/associativeArray3.php
@@ -0,0 +1,11 @@
+<?php  
+
+$group = array("A"=> 500, "B"=> 400, "C"=> 300, "D"=> 200, "E"=> 100);
+
+echo "Group A Score : ".$group['A']."<br>";
+echo "Group B Score : ".$group['B']."<br>";
+echo "Group C Score : ".$group['C']."<br>";
+echo "Group D Score : ".$group['D']."<br>";
+echo "Group E Score : ".$group['E']."<br>";
+
+?>
