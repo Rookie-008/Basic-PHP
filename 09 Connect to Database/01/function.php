5854e80dc449e09ee9adbf653bb7906839eedb19
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2025-12-19 06:32:06 +0700

    Update function.php

--- 09 Connect to Database/01/function.php
+++ 09 Connect to Database/01/function.php
@@ -1,7 +1,8 @@
 <?php
 // Connection to Database
 // mysqli_connect("Host Name", "Username MySQL", "Password", "Database Name");
-$db_connect = mysqli_connect("localhost", "root", "12345", "db_name");  
+$db_connect = mysqli_connect("localhost", "root", "12345", "db_name");
+//$db_connect = mysqli_connect("localhost", "root", "836967826984", "db_name"); 
 
 function query($query) {
 	global $db_connect;
@@ -12,4 +13,5 @@ function query($query) {
 	}
 	return $data;
 }
+
 ?>
