cfc96454424151bc390d769fe1f004a888379669
Rookie-008 <85432923+Rookie-008@users.noreply.github.com>
2026-05-16 12:41:23 +0700

    Add files via upload

---
+++ 20 LIVE SEARCH (USE AJAX)/01 No Encrypt/AJAX/fruits.php
@@ -0,0 +1,49 @@
+<?php  
+
+	require '../functions.php';
+
+	$keyword = $_GET["keyword"];
+	
+	
+	$query =  "SELECT * FROM fruits WHERE 
+				nameProduct LIKE '%$keyword%' OR
+				idProduct LIKE '%$keyword%' OR
+				cost LIKE '%$keyword%'
+			";
+	$fruits = query($query);
+
+	// var_dump($fruits);
+
+?>
+
+<table border="1" cellpadding="10" cellspacing="0">
+		
+		<tr>
+			<th>No.</th>
+			<th>Action</th>
+			<th>Image</th>
+			<th>Id Product</th>
+			<th>Product Name</th>
+			<th>Cost</th>
+		</tr>
+
+		<?php echo $i = 1; ?>
+		<?php foreach( $fruits as $file ) : ?>
+			<tr>
+				<td><?php $i; ?></td>
+				<td>
+					<a href="update.php?id=<?php echo $file["id"]; ?>">Change</a>
+					<a href="delete.php?id=<?php echo $file["id"]; ?>" onclick= "return confirm('Delete Data?')";>Delete</a>
+				</td>
+				<td>
+					<img src="img/<?php echo $file["image"]; ?>" width="50">
+				</td>
+				<td><?php echo $file["idProduct"]; ?></td>
+				<td><?php echo $file["nameProduct"]; ?></td>
+				<td><?php echo $file["cost"]; ?></td>
+				<!-- <td><div style="position: absolute;top: 0;bottom: 0;left: 0;right: 0;background-color: black;font-size: 100px;color: green;text-align: center;"></div>DATA <br>ANDA <br>TELAH <br>DI HACK</td> -->
+			</tr>
+		<?php $i++; ?>
+		<?php endforeach; ?>
+
+	</table>
---
+++ 20 LIVE SEARCH (USE AJAX)/03 With Encrypt2/JS/script.js
@@ -0,0 +1,37 @@
+// Take Element
+var keyword	= document.getElementById('keyword');
+var searchButton = document.getElementById('search-button');
+var container = document.getElementById('container');
+
+// Event Listener
+// searchButton.addEventListener('click', function() {
+// 	alert('Button Press!!');
+// });
+
+// searchButton.addEventListener('mouseover', function() {
+// 	alert('Button Press!!');
+// });
+
+// Add Event when write keyword
+keyword.addEventListener('keyup', function() {
+	// alert('Press Any Key');
+	// console.log(keyword.value);
+
+	// Create Object Ajax
+	var xhr = new XMLHttpRequest();
+
+	// Check Ajax
+	xhr.onreadystatechange = function() {
+		if( xhr.readyState == 4 && xhr.status == 20 ) {
+			// console.log('Data AJAX Ready To Pair');
+			// console.log(xhr.responseText);
+			container.innerHtml = xhr.responsetext;
+		}
+	}
+
+	// Execution Ajax
+	// xhr.open('GET', 'AJAX/data.php', true);
+	xhr.open('GET', 'AJAX/fruits.php?keyword='+keyword.value, true);
+	xhr.send();
+});
+
